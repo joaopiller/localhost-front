a5df6b7347bd9557a952aae65b5c263658b42b9b
João Vicente Piller Menezes <joaovicentepm14@gmail.com>
2024-11-17 12:20:48 -0300

    feat: header components

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,20 @@
+import logo from '../../assets/logo.svg';
+import Image from 'next/image';
+import Link from 'next/link';
+
 export default function Header() {
     return (
         <header>
-            
+            <Image
+                src={logo}
+                alt="Localhost Logo"
+                width={156}
+                height={40}
+            />
+            <nav>
+                <Link href='/'>Home</Link>
+                <Link href='/sign-in'>Sign In</Link>
+            </nav>
         </header>
     )
 }
