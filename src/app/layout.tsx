dc6da3a0211861d9ddd71f53c278fc5e54bda88f
João Vicente Piller Menezes <joaovicentepm14@gmail.com>
2024-11-17 12:37:03 -0300

    feat: header style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,22 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Header from "@/components/Header";
+import { Archivo } from 'next/font/google'
 
 export const metadata: Metadata = {
   title: "Localhost",
   description: "Localhost Website",
 };
 
+const archivo = Archivo({
+  subsets: ['latin'],
+  variable: '--archivo',
+  display: 'swap',
+})
+
 export default function RootLayout({children,}: Readonly<{children: React.ReactNode;}>) {
   return (
-    <html lang="en">
+    <html lang="en" className={archivo.variable}>
       <body>
         <Header/>
         {children}
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,23 @@
 import logo from '../../assets/logo.svg';
 import Image from 'next/image';
 import Link from 'next/link';
+import styles from './styles.module.css';
 
 export default function Header() {
     return (
-        <header>
-            <Image
-                src={logo}
-                alt="Localhost Logo"
-                width={156}
-                height={40}
-            />
-            <nav>
-                <Link href='/'>Home</Link>
-                <Link href='/sign-in'>Sign In</Link>
-            </nav>
+        <header className={styles.wrapper}>
+            <div>
+                <Image
+                    src={logo}
+                    alt="Localhost Logo"
+                    width={156}
+                    height={40}
+                />
+                <nav>
+                    <Link className={styles.link} href='/'>Home</Link>
+                    <Link className={styles.link} href='/sign-in'>Sign In</Link>
+                </nav>
+            </div>
         </header>
     )
 }
