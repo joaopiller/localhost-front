6450d4ee1be3035df44344eee54e22e344204dd1
João Vicente Piller Menezes <joaovicentepm14@gmail.com>
2024-11-18 21:54:58 -0300

    feat: acommodation card and reset css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,28 @@
 import styles from "./page.module.css";
+import example from "../assets/acommodation.jpg";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
+    <div style={{backgroundColor: 'gray', height: '1000px', display: 'flex', justifyContent:'center', alignItems:'center'}}>
+      <div className={styles.wrapper}>
+        <div
+          className={styles.bannerSection}
+          style={{
+            backgroundImage: `url(${example.src})`,
+          }}
+        >
+        </div>
+        <div className={styles.infoSection}>
+          <div>
+            <h3>Urban Loft</h3>
+            <p>Giorgina Bay, Ontario, California</p>
+          </div>
+          <div>
+            <span></span>
+            <span>/night</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
