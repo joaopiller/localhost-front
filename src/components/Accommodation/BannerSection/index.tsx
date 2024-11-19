ff7fe7910c9d9054f69e26d990d5503504ddfe2b
João Vicente Piller Menezes <joaovicentepm14@gmail.com>
2024-11-18 23:02:22 -0300

    refactor: icons folder

--- src/components/Accommodation/BannerSection/index.tsx
+++ src/components/Accommodation/BannerSection/index.tsx
@@ -1,7 +1,7 @@
 import styles from "./styles.module.css";
 import Image from "next/image";
-import deleteIcon from "../../../assets/delete.svg";
-import edit from "../../../assets/edit.svg";
+import deleteIcon from "../../../assets/icons/delete.svg";
+import edit from "../../../assets/icons/edit.svg";
 
 export default function BannerSection({image}: {image: string}) {
     return (
