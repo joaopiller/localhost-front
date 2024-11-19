54188742b0406268b08b59a94a269c9a13b6c4f8
João Vicente Piller Menezes <joaovicentepm14@gmail.com>
2024-11-19 14:53:24 -0300

    fix: edit and delete button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
+import Accommodation from "@/components/Accommodation";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.page}>
+      <Accommodation
+        title="Accommodation"
+        location="Bali, Indonesia"
+        image="https://casaecampo.com.br/wp-content/uploads/2023/09/f0c6e919-9122-4e5c-a077-acf511924ab8.jpeg"
+        price="100"
+      />
     </div>
   );
 }
--- src/components/Accommodation/BannerSection/index.tsx
+++ src/components/Accommodation/BannerSection/index.tsx
@@ -11,22 +11,20 @@ export default function BannerSection({image}: {image: string}) {
                 backgroundImage: `url(${image})`,
             }}
         >
-            <button className={`${styles.iconBtn} ${styles.edit}`}>
-                <Image
-                    src={edit}
-                    alt="Edit Icon"
-                    width={25}
-                    height={25}
-                />
-            </button>
-            <button className={styles.iconBtn}>
-                <Image
-                    src={deleteIcon}
-                    alt="Delete Icon"
-                    width={25}
-                    height={25}
-                />
-            </button>
+            <Image
+                src={edit}
+                alt="Edit Icon"
+                width={25}
+                height={25}
+                className={`${styles.iconBtn} ${styles.edit}`}
+            />
+            <Image
+                src={deleteIcon}
+                alt="Delete Icon"
+                width={25}
+                height={25}
+                className={styles.iconBtn}
+            />
         </div>
     )
 }
