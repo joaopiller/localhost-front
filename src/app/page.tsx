e637f3f2f0f4b2ce35ee6b6959c6079bb3c11686
João Vicente Piller Menezes <joaovicentepm14@gmail.com>
2024-11-18 23:03:56 -0300

    feat: accommodation component done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-import Accommodation from "@/components/Accommodation";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <Accommodation image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFg9lIo9D1LEh4_4p0V9LvFk2LKj4WvjqsYw&s" title="Casinha" location="Rua Antonio, Nova Friburgo, RJ" price="100"/>
     </div>
   );
 }
