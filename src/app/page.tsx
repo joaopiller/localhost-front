e0d4ff1fd3dde50f5536c7401e54054d8e6ee8be
João Vicente Piller Menezes <joaovicentepm14@gmail.com>
2024-11-25 13:23:35 -0300

    feat: first structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,29 @@
+import GreenButton from "@/components/GreenButton";
 import styles from "./page.module.css";
+import Image from "next/image";
+import search from "../assets/icons/search.svg";
 
 export default function Home() {
   return (
     <div className={styles.page}>
+      <section className={styles.searchSection}>
+        <div className={styles.wrapper}>
+          <h1>Find a <span>host</span> for everty journey</h1>
+          <p>Discover the best local rental properties that fits your every travel needs</p>
+          <div className={styles.searchBar}>
+            <input type="text" placeholder="Accommodation"/>
+            <GreenButton>
+              <Image
+                src={search}
+                alt="Search"
+                width={20}
+                height={20}
+              />
+              Search
+            </GreenButton>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
