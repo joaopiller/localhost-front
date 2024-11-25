71857a0b0843440fe8639a7574dd7b536b6038bd
João Vicente Piller Menezes <joaovicentepm14@gmail.com>
2024-11-25 14:00:41 -0300

    feat: filter section styled

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,27 @@ import GreenButton from "@/components/GreenButton";
 import styles from "./page.module.css";
 import Image from "next/image";
 import search from "../assets/icons/search.svg";
+import houseIcon from "../assets/icons/house.svg";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <section className={styles.searchSection}>
         <div className={styles.wrapper}>
-          <h1>Find a <span>host</span> for everty journey</h1>
+          <h1>Find a <span>host</span> for every journey</h1>
           <p>Discover the best local rental properties that fits your every travel needs</p>
           <div className={styles.searchBar}>
-            <input type="text" placeholder="Accommodation"/>
-            <GreenButton>
+            <div className={styles.searchInput}>
+              <input type="text" placeholder="Accommodation"/>
+              <Image
+                  src={houseIcon}
+                  alt="House Icon"
+                  width={25}
+                  height={25}
+                  className={styles.inputIcon}
+                />
+            </div>
+            <GreenButton className={styles.searchButton}>
               <Image
                 src={search}
                 alt="Search"
