096db1bcee16a1e442e6b609733cc6ff39439b2f
João Vicente Piller Menezes <joaovicentepm14@gmail.com>
2024-11-25 23:30:47 -0300

    feat: contact section structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,22 @@ export default function Home() {
     <div className={styles.page}>
       <FilterSection/>
       <AccommodationsSection/>
+      <section className={styles.contactSection}>
+        <div className={styles.contactTexts}>
+          <h2>Contact</h2>
+          <p>Contact the Localhost team for questions or assistance with your stay</p>
+        </div>
+        <div className={styles.contactForm}>
+          <h3>Fill out the form</h3>
+          <div className={styles.inputsSection}>
+            <input className={styles.textInput} type="text" placeholder="Name"/>
+            <input className={styles.textInput} type="email" placeholder="Email"/>
+            <input className={styles.textInput} type="tel" placeholder="Phone number"/>
+            <textarea className={styles.textInput} style={{height: "5.813rem"}} name="quentions" id="quentions" placeholder="Questions"></textarea>
+          </div>
+          <button>Send</button>
+        </div>
+      </section>
     </div>
   );
 }
